6523e4ee83af2a32fbbf5955e8916254f99575d3
xujh5 <xujh5@lenovo.com>
2019-05-07 16:15:33 +0800

    添加项目文件。

---
+++ DownLoadDB/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace DownLoadDB
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(41, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(98, 21);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 21);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(213, 30);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 12);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "label2";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(270, 21);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 21);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(396, 29);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 12);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "label3";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(457, 21);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 21);
+			this.textBox3.TabIndex = 5;
+			this.textBox3.Leave += new System.EventHandler(this.TextBox3_Leave);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(41, 67);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(41, 12);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "label4";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(98, 58);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 20);
+			this.comboBox1.TabIndex = 8;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(250, 66);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(41, 12);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "label5";
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(298, 57);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 20);
+			this.comboBox2.TabIndex = 10;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(439, 66);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(41, 12);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "label6";
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Location = new System.Drawing.Point(487, 56);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(121, 20);
+			this.comboBox3.TabIndex = 12;
+			this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.ComboBox3_SelectedIndexChanged);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(43, 96);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(666, 145);
+			this.groupBox1.TabIndex = 13;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
+}
+
---
+++ DownLoadDB/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DownLoadDB
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public void BindDB()
+		{
+			string connStr = @"Data Source="+textBox1.Text+";Initial Catalog=master;Persist Security Info=True;User ID="+textBox2.Text+";Password="+textBox3.Text+"";
+			try
+			{
+				string strSql = @"select name AS id, name AS text From dbo.sysdatabases";
+				List<string> listDB = GetList(connStr, strSql).ToList();
+				comboBox1.DataSource = listDB;
+				comboBox1.DisplayMember = "text";
+			}
+		   catch(Exception ex)
+			{
+				MessageBox.Show(""+ex.Message+"！");
+			}
+		}
+
+		private void TextBox3_Leave(object sender, EventArgs e)
+		{
+			BindDB();
+		}
+		
+		/* select name from sysobjects where xtype='u' ---
+C = CHECK 约束
+D = 默认值或 DEFAULT 约束
+F = FOREIGN KEY 约束
+L = 日志
+FN = 标量函数
+IF = 内嵌表函数
+P = 存储过程
+PK = PRIMARY KEY 约束（类型是 K）
+RF = 复制筛选存储过程
+S = 系统表
+TF = 表函数
+TR = 触发器
+U = 用户表
+UQ = UNIQUE 约束（类型是 K）
+V = 视图
+X = 扩展存储过程
+*/
+		public string[] GetTableList(string connString)
+		{
+			string strSql = "SELECT name FROM sysobjects WHERE xtype='U' AND name  <>  'dtproperties' order by name asc";
+			return GetList(connString,strSql);
+		}
+		public string[] GetPROList(string connString)
+		{
+			string strSql = "SELECT name FROM sysobjects WHERE xtype='P' AND name  <>  'dtproperties' order by name asc";
+			return GetList(connString, strSql);
+		}
+		private string[] GetList(string connString, string sql)
+		{
+			if (String.IsNullOrEmpty(connString)) return null;
+			string connStr = connString;
+			SqlConnection conn = new SqlConnection(connStr);
+			SqlCommand cmd = new SqlCommand(sql, conn);
+			cmd.CommandType = CommandType.Text;
+			cmd.CommandTimeout = 600;
+			try
+			{
+				conn.Open();
+				List<string> ret = new List<string>();
+				using (SqlDataReader MyReader = cmd.ExecuteReader())
+				{
+					while (MyReader.Read())
+					{
+						ret.Add(MyReader[0].ToString());
+					}
+				}
+				if (ret.Count > 0) return ret.ToArray();
+				return null;
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
+
+		private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string strDB = comboBox1.Text;
+			string connStr = @"Data Source=" + textBox1.Text + ";Initial Catalog=" + strDB + ";Persist Security Info=True;User ID=" + textBox2.Text + ";Password=" + textBox3.Text + "";
+			try
+			{
+				List<string> listTable = GetTableList(connStr).ToList();
+				List<string> listPRO = GetPROList(connStr).ToList();
+				comboBox2.DataSource = listTable;
+				comboBox3.DataSource = listPRO;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("" + ex.Message + "！");
+			}
+		}
+		
+		private void ComboBox3_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string strDB = comboBox1.Text;
+			string connStr = @"Data Source=" + textBox1.Text + ";Initial Catalog=" + strDB + ";Persist Security Info=True;User ID=" + textBox2.Text + ";Password=" + textBox3.Text + "";
+			SqlConnection conn = new SqlConnection(connStr);
+			SqlCommand cmd = new SqlCommand();
+			cmd.Connection = conn;
+			cmd.CommandText = "" + comboBox3.Text + "";
+			cmd.CommandType = CommandType.StoredProcedure;
+			try
+			{
+				conn.Open();
+				SqlCommandBuilder.DeriveParameters(cmd);
+				int i = 0;
+				foreach (Control item in groupBox1.Controls)
+				{
+					groupBox1.Controls.Remove(item);
+				}
+				
+				foreach (SqlParameter var in cmd.Parameters)
+				{
+					if (cmd.Parameters.IndexOf(var) == 0) continue;//Skip return value
+
+					Label label = new Label();
+					label.Text = var.ParameterName.Replace("@","");
+					label.Name = var.ParameterName;
+					label.Location = new Point(30+i, 50);
+					TextBox textBox = new TextBox();
+					textBox.Name = "txt" + var.ParameterName.Replace("@", "");
+					textBox.Location = new Point(100+i, 50);
+					groupBox1.Controls.Add(label);
+					groupBox1.Controls.Add(textBox);
+					i += 100;
+				}
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
+	}
+}
